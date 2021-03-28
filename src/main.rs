c68ca1586b4e6507e4283ca612cf62f2ed86ea99
linuxing3 <linuxing3@qq.com>
2021-03-28 11:27:10 -0400

    added cli and gui

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 use iced::{Settings, Application};
-
 use rs-vpsman::gui::Counter;
+use clap::{App, load_yaml};
 
-pub fn main() -> iced::Result {
+fn main() {
+    let yaml = load_yaml!("config.yml");
+    let opts = App::from(yaml).get_matches();
+
+    println!("Value for config: {}", opts.config);
+    println!("Using input file: {}", opts.input);
+
+    match opts.verbose {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    match opts.gui {
+        _ => gui(),
+    }
+}
+
+pub fn gui() -> iced::Result {
     <Counter as Application>::run(Settings::default())
 }
 
